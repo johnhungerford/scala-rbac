de884e73024d3e56176fe99911c9ce920d8aed82
John Hungerford <john.hungerford@twosixlabs.com>
2021-01-14 06:18:07 -0500

    fixed scalatra example scalatra init path in main, and made port same as play app

--- scala-rbac-examples/scalatra-example/src/main/scala/Main.scala
+++ scala-rbac-examples/scalatra-example/src/main/scala/Main.scala
@@ -8,14 +8,14 @@ object Main {
 
     def main( args : Array[ String ] ) : Unit = {
 
-        val port : Int = Try( args( 0 ).toInt ).getOrElse( 8080 )
+        val port : Int = Try( args( 0 ).toInt ).getOrElse( 9000 )
 
         val server = new Server ( port )
         val context = new WebAppContext()
 
         context.setContextPath( "/" )
         context.setResourceBase( "src/main/webapp" )
-        context.setInitParameter( ScalatraListener.LifeCycleKey, "org.hungerford.rbac.example.ScalatraInit")
+        context.setInitParameter( ScalatraListener.LifeCycleKey, "org.hungerford.scalarbac.example.ScalatraInit")
         context.addEventListener( new ScalatraListener )
 
         server.setHandler( context )
